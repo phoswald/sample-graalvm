f08af20beba6a1d9a798c418bae9676821dd4931
Philip Oswald <philip.oswald@gmail.com>
2025-03-08 01:57:32 +0100

    Exec native-image directly, assuming it supports musl

--- build-native-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export MUSL_HOME="${HOME}/bin/musl"
-export PATH="${MUSL_HOME}/bin/:${PATH}"
-native-image \
-  --no-fallback --static --libc=musl \
-  -cp target/sample-graalvm-*.jar \
-  com.github.phoswald.sample.Application \
-  target/sample-graalvm
